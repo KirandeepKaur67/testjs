f0c4f9d7726fd9186ad6d119bbc6c715a4ec4629
KirandeepKaur67 <162323113+KirandeepKaur67@users.noreply.github.com>
2024-03-07 12:12:30 +0000

    day2,3

---
+++ day2/day2.js
@@ -0,0 +1,7 @@
+const accountId = 123456;
+let accountEmail = "demo@gmail.com";
+var accountPassword = "0123456";
+// const accountId = 12;
+accountEmail="js@gmail.com";
+let accountCity ="jaipur";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
